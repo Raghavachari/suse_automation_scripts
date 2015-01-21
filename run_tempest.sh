99094b5c73fb2c83b669b2792d2cc967e8d1d2b2
rmulugu <rmulugu@infoblox.com>
2015-01-21 02:24:56 -0800

    run tempest script individually instead of complet folder

--- run_tempest.sh
+++ run_tempest.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cd /root/tempest/
-nosetests -v tempest/thirdparty/infoblox/scenarios/*
-
+nosetests -v tempest.thirdparty.infoblox.scenarios.test_ext_floating_ip
+sleep 5
+nosetests -v tempest.thirdparty.infoblox.scenarios.test_create_del_snets
+sleep 5
+nosetests -v tempest.thirdparty.infoblox.scenarios.test_snet_range
+sleep 5
+nosetests -v tempest.thirdparty.infoblox.scenarios.test2
+sleep 5
+nosetests -v tempest.thirdparty.infoblox.scenarios.test_scenario
