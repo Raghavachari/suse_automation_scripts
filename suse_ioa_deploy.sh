ba5a9b734886314d213945833adc06f9d33fbf0d
rmulugu <rmulugu@infoblox.com>
2015-01-28 05:35:14 -0800

    enabled dhcp relay support for suse

--- suse_ioa_deploy.sh
+++ suse_ioa_deploy.sh
@@ -4,7 +4,7 @@ DEPLOYMENT_SERVICES=ioa
 IP=$1
 ib_patch=$2
 wapi=https://192.168.124.159/wapi/v1.4.1
-members='[{"ipv4addr": "192.168.124.159", "name": "member.com"}, {"ipv4addr": "192.168.124.162", "name": "member2.com"}, {"ipv4addr": "192.168.124.163", "name": "member3.com"}]'
+members='[{"ipv4addr": "192.168.124.161", "name": "member1.com"}, {"ipv4addr": "192.168.124.162", "name": "member2.com"}, {"ipv4addr": "192.168.124.163", "name": "member3.com"}]'
 if [ x$IP == x ]; then
 echo "IP address is not specified"
 fi
@@ -14,7 +14,7 @@ echo "patch is not specified"
 exit 0
 fi
 
-eth_port=`ifconfig -a |grep eth0 |awk '{print $1}' | sed s/\\..*// | sort -u`
+eth_port=`ifconfig -a |grep eth |awk '{print $1}'|head -1`
 patch_file=`basename $ib_patch`
 zypper in expect
 sleep 10
@@ -23,8 +23,10 @@ patch_folder=`echo $patch_file | sed -r 's/\.[[:alnum:]]+\.[[:alnum:]]+$//'`
 tar -mxf $patch_file
 f1="/root/$patch_folder/ib_localrc"
 f2="/root/$patch_folder/ib_ioarc"
+f3="/root/$patch_folder/ib_deploy.sh"
 
 sed -i 's/export DEPLOYMENT_SERVICES=.*/export DEPLOYMENT_SERVICES='$DEPLOYMENT_SERVICES'/' $f1
+sed -i '1 a\cd /root/'$patch_folder'/' $f3
 
 sed -i 's+export\ WAPI=.*+export\ WAPI=https:\/\/192.168.124.159\/wapi\/v1.4.1\/+' $f2
 sed -i "s/export IB_MEMBERS=.*/export IB_MEMBERS='$members'/" $f2
@@ -33,7 +35,11 @@ sed -i 's/export IOA_HOST_GATEWAY=.*/export IOA_HOST_GATEWAY=192.168.124.1/' $f2
 sed -i 's/export IOA_HOST_NETMASK=.*/export IOA_HOST_NETMASK=255.255.255.0/' $f2
 sed -i 's/export IOA_HOST_DNS=.*/export IOA_HOST_DNS=192.168.124.159/' $f2
 sed -i 's/export IOA_INTERFACE=.*/export IOA_INTERFACE='$eth_port'/' $f2                                                                        
-sed -i 's/IOA_SKIP_IPV6_DISABLING=.*/IOA_SKIP_IPV6_DISABLING=true/' $f2
+sed -i 's/IOA_SKIP_IPV6_DISABLING=.*/IOA_SKIP_IPV6_DISABLING=false/' $f2
+sed -i 's/IOA_SKIP_RELAY=.*/IOA_SKIP_RELAY=false/' $f2
+sed -i 's/IOA_SKIP_NETWORK_CONFIGURATION=.*/IOA_SKIP_NETWORK_CONFIGURATION=false/' $f2
+sed -i 's/export IOA_DHCP_RELAY_MANAGEMENT_NETWORK=.*/export IOA_DHCP_RELAY_MANAGEMENT_NETWORK=192.168.124.0\/24/' $f2
+sed -i 's/export IOA_DHCP_RELAY_MANAGEMENT_NETWORK_VIEW=.*/export IOA_DHCP_RELAY_MANAGEMENT_NETWORK_VIEW=test_mgmt/' $f2
 echo "file patched sucessfully going to deploy..."
 
 echo "Tempest Pre-requisite packages install.."
